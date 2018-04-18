e45c546fc69d3dc3b574d8e9f6455594e7c2faa3
Frandi Dwi <dev@rumahfrandi.com>
2018-04-18 17:34:52 +0700

    Add project files.

---
+++ PG.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace PG.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ PG.Api/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PG.Api.WebApiApplication" Language="C#" %>
